33969094b1b9aeb045310bcd3d518445fa6b1d01
Sumit Gupta <119912107+Sumit-1018@users.noreply.github.com>
2024-04-22 19:24:28 +0530

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,28 @@
+makeCacheMatrix <- function(x = matrix()) {
+  # var and functions
+  matrizinversa <- NULL
+  set <- function( y ){
+    x <<- y
+    matrizinversa <<- NULL
+  }
+  get <- function() ( x )
+  setInversa <- function( calculoinversa ) ( matrizinversa <<- calculoinversa )
+  getInversa <- function() ( matrizinversa )
+  list( set = set, get = get, setInversa = setInversa, getInversa = getInversa )
+  
+}
+
+
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  solinversa <- x$getInversa()
+  if (!is.null(solinversa)) {
+    message( "getting cached data" )
+    return( solinversa )
+  }
+  data <- x$get()
+  solinversa <- solve(data, ...)
+  x$setInversa(solinversa)
+  solinversa
+}
